6e30208fa8de0063355de346403a900144ce0b41
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-17 15:47:05 +0900

    ストロングパラメーター修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in, keys: :name)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
