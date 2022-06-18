fbac3d95905e9bf7e504d3afd0dc9593173a8bf1
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-18 14:18:00 +0900

    charcters#createアクション作成

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -6,4 +6,17 @@ class CharactersController < ApplicationController
   def new
     @character = Character.new
   end
+
+  def create
+    if Character.create(character_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def character_params
+    params.require(:character).permit(:name, :url, :describe).merge(user_id: current_user.id)
+  end
 end
