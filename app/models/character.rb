2429903db815589f230a4b958245f2c10d17e21b
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-18 13:05:57 +0900

    Characterモデルバリデーション、アソシエーション設定

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,9 @@
 class Character < ApplicationRecord
+  belongs_to :user
+  # has_many :past_topics, dependent: destroy
+  # has_many :future_topics, dependent: destroy
+  # has_many :categories, through: character_categories, dependent: destroy
+
+  validates :name
+
 end
