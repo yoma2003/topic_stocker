98394fd55f22722a713f4d598d90fded7fd60233
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-18 16:17:15 +0900

    FactoryBot編集

--- spec/factories/characters.rb
+++ spec/factories/characters.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :character do
-    
+    name                  {Faker::Name.last_name}
+    url                   {Faker::Internet.url}
+    describe              {Faker::Lorem.sentence}
+    association :user
+
+    after(:build) do |character|
+      character.image.attach(io:File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
