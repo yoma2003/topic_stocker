4ee49b3098ff591d9bfb00a6d8940c689b544f37
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-18 13:18:09 +0900

    Characterモデルマイグレーション再実行

--- db/migrate/20220618035834_create_characters.rb
+++ db/migrate/20220618035834_create_characters.rb
@@ -1,10 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
     create_table :characters do |t|
-      t.string    :name,    null: false
+      t.string    :name,  null: false
       t.string    :url
       t.text      :describe
-      t.reference :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_055519) do
+ActiveRecord::Schema.define(version: 2022_06_18_035834) do
+
+  create_table "characters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "url"
+    t.text "describe"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_characters_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 2022_06_17_055519) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "characters", "users"
 end
