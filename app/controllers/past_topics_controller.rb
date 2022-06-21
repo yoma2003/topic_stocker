2eac02278d8d5ef244063e654843356866661977
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-21 17:19:58 +0900

    past_topics#updateアクションの作成

---
+++ app/controllers/past_topics_controller.rb
@@ -0,0 +1,13 @@
+class PastTopicsController < ApplicationController
+  def update
+    latest_past_topic = PastTopic.find(params[:id])
+    latest_past_topic.update(past_topic_params)
+    redirect_to root_path
+  end
+
+  private
+  
+  def past_topic_params
+    params.require(:past_topic).permit(:past_topic, :created_date, :character_id)
+  end
+end
