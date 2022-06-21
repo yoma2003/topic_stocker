354785fae99d55e016fc47c7d7b9ccd754a64f0e
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-21 14:22:50 +0900

    PastTopiモデルのマイグレーション修正

--- db/migrate/20220621044321_create_past_topics.rb
+++ db/migrate/20220621044321_create_past_topics.rb
@@ -1,7 +1,7 @@
 class CreatePastTopics < ActiveRecord::Migration[6.0]
   def change
     create_table :past_topics do |t|
-      t.text       :topic
+      t.text       :past_topic
       t.date       :created_date, null: false
       t.references :character, null: false, foreign_key: true
 
