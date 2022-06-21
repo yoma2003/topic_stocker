f957f9698210ac2660dc9f87c60a72abbbc36b60
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-21 17:21:49 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: "characters#index"
   resources :users, only: [:edit, :update]
   resources :characters
+  resources :past_topics, only: :update
+  resources :future_topics, only: :update
 end
