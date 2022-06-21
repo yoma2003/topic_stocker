8fe26fa3dca6afd82020d3733f4fa83bb7825ba1
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-21 12:24:41 +0900

    characters#destroyアクションの作成

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -30,6 +30,12 @@ class CharactersController < ApplicationController
     end
   end
 
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+    redirect_to root_path
+  end
+
   private
 
   def character_params
