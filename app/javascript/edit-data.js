6bffaf83edc70996dd48eba19a6ee149ccfd1c2c
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-22 16:45:57 +0900

    characters/indexビューJS編集

--- app/javascript/edit-data.js
+++ app/javascript/edit-data.js
@@ -1,19 +1,38 @@
 const editData = () => {
   const characters = document.querySelectorAll(".character")
-  characters.forEach(function(character) {
-    
+  characters.forEach(function(character) {    
+    character.addEventListener("click", function(e) {
+      const pastTopicInput = this.children[1].children[0].children[0].children[3];
+      const futureTopicInput = this.children[1].children[1].children[0].children[3];
 
-    character.addEventListener("click", function() {
       const editedCharacter = document.querySelector(".edit")
+
       if (this.classList.contains("edit") == true) {
-        this.classList.remove("edit");
+        e.preventDefault();
       } else if (editedCharacter != null) {
         editedCharacter.classList.remove("edit");
+        editedCharacter.children[1].children[0].children[0].children[3].setAttribute("disabled", true);
+        editedCharacter.children[1].children[1].children[0].children[3].setAttribute("disabled", true);
+
         this.classList.add("edit");
+        pastTopicInput.removeAttribute("disabled");
+        futureTopicInput.removeAttribute("disabled");
+
       } else {
         this.classList.add("edit");
+        pastTopicInput.removeAttribute("disabled");
+        futureTopicInput.removeAttribute("disabled");
       }
     });
+
+    // character.addEventListener("blur", function() {
+    //   const pastTopicInput = this.children[1].children[0].children[0].children[3];
+    //   const futureTopicInput = this.children[1].children[1].children[0].children[3];
+
+    //   this.classList.remove("edit");
+    //   pastTopicInput.setAttribute("disabled", true);
+    //   futureTopicInput.setAttribute("disabled", true);
+    // });
   });
 };
 
