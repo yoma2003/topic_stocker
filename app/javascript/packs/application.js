246b7ea6b919386383673725eab9223efd1b281f
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-23 17:22:40 +0900

    flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,13 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("edit-data")
-require("update-topic")
+require("../edit-data")
+require("../update-topic")
+require("flatpickr")
 
-
+document.addEventListener('DOMContentLoaded', function() {
+  flatpickr('#flatp');
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
