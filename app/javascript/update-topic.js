156c30bf5b858d78d050f4c7bb44d63bf91d17e1
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-23 18:22:32 +0900

    update-topicJS修正

--- app/javascript/update-topic.js
+++ app/javascript/update-topic.js
@@ -3,32 +3,32 @@ const updateTopic = () => {
   const futureTopicInputs = document.querySelectorAll(".future_topic_input");
   pastTopicInputs.forEach(function(pastTopicInput){
     pastTopicInput.addEventListener("blur", function(e){
-      updateData(pastTopicInput);
+      updateData(pastTopicInput, "past_topics");
     });
     pastTopicInput.addEventListener("focus", function(){
       window.addEventListener("beforeunload", function(e){
-        updateData(pastTopicInput);
+        updateData(pastTopicInput, "past_topics");
       });
     });
   });
   futureTopicInputs.forEach(function(futureTopicInput){
     futureTopicInput.addEventListener("blur", function(e){
-      updateData(futureTopicInput);
+      updateData(futureTopicInput, "future_topics");
     });
     futureTopicInput.addEventListener("focus", function(){
       window.addEventListener("beforeunload", function(e){
-        updateData(futureTopicInput);
+        updateData(futureTopicInput, "future_topics");
       });
     });
   });
 };
 
-const updateData = (topicInput) => {
+const updateData = (topicInput, controller) => {
   const form = topicInput.parentElement;
   const form_id = form.getAttribute("data")
   const formData = new FormData(form);
   const XHR = new XMLHttpRequest();
-  XHR.open("PUT", `/future_topics/${form_id}`, true);
+  XHR.open("PUT", `/${controller}/${form_id}`, true);
   XHR.send(formData);
 };
 
