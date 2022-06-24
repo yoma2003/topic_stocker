89f6162c7b66bed2edfe5ec37c6845b018ef90c6
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-24 17:23:43 +0900

    Revert "flatpickrの導入"
    This reverts commit 17faa8a0ebf0ded276006341f392cd154f8e0c0a.

--- app/javascript/packs/calendar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("turbolinks:load", () => {
-  flatpickr.localize(flatpickr.l10ns.ja)
-  const config = {
-    inline: true,
-    disableMobile: true
-  }
-  flatpickr('#calendar_form', config);
-});
