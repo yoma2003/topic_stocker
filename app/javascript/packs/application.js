b552372d6b80642ba72f386d8f762366db7d205c
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-24 17:23:36 +0900

    Revert "微修正"
    This reverts commit 7af061510c7e9415ddcfc52679ff0271e20da415.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../edit-data")
-require("../update-topic")
 require('flatpickr')
 require('flatpickr/dist/l10n/ja')
 
