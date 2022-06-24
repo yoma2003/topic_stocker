7af061510c7e9415ddcfc52679ff0271e20da415
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-24 17:23:31 +0900

    微修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../edit-data")
+require("../update-topic")
 require('flatpickr')
 require('flatpickr/dist/l10n/ja')
 
