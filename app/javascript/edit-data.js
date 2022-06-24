f857c76eaa52ad1914826e24447d1fbec0a84d02
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-24 20:14:09 +0900

    edit-dataJS修正

--- app/javascript/edit-data.js
+++ app/javascript/edit-data.js
@@ -1,17 +1,18 @@
 const editData = () => {
   const characters = document.querySelectorAll(".character")
   characters.forEach(function(character) {    
-    character.addEventListener("click", function(e) {
+    character.addEventListener("click", function() {
       const pastTopicInput = this.children[1].children[0].children[0].children[3];
       const futureTopicInput = this.children[1].children[1].children[0].children[3];
 
       const editedCharacter = document.querySelector(".edit")
-
-      if (editedCharacter != null) {
+      if (this.classList.contains("edit") == true) {
+        return;
+      } else if (editedCharacter != null) {
         editedCharacter.classList.remove("edit");
         editedCharacter.children[1].children[0].children[0].children[3].setAttribute("disabled", true);
         editedCharacter.children[1].children[1].children[0].children[3].setAttribute("disabled", true);
-
+        console.log("OK")
         this.classList.add("edit");
         pastTopicInput.removeAttribute("disabled");
         futureTopicInput.removeAttribute("disabled");
