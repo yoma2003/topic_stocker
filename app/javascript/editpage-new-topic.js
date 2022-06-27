7255b97544eeb7b5cdd8934fe2d054a25a677b66
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-27 18:15:38 +0900

    editpage-new-topicJSに削除ボタン処理を追加

--- app/javascript/editpage-new-topic.js
+++ app/javascript/editpage-new-topic.js
@@ -1,4 +1,5 @@
 import { updateData } from "./update-topic";
+import { deleteData } from "./delete-topic";
 
 const newTopic = () => {
   const newTopicButton = document.getElementById("new_topic_btn");
@@ -23,16 +24,20 @@ const newData = (csrfToken, characterId) => {
   XHR.onload = () => {
     const newTopic = XHR.response.new_past_topic; // 新規フォームデータの取得
     const html = `
-    <form class="topic_form" data="${newTopic.id}" action="/past_topics/${newTopic.id}" accept-charset="UTF-8" method="post">
-      <input type="hidden" name="_method" value="patch">
-      <input type="hidden" name="authenticity_token" value="${csrfToken}">
-      <input value="${newTopic.created_date}" type="date" name="past_topic[created_date]" id="past_topic_created_date">              
-      <textarea class="new_past_topic_input" name="past_topic[past_topic]" id="past_topic_past_topic"></textarea>
-    </form>
-    `;
+    <div id="past_topic_${newTopic.id}">
+      <form class="topic_form" data="${newTopic.id}" action="/past_topics/${newTopic.id}" accept-charset="UTF-8" method="post">
+        <input type="hidden" name="_method" value="patch">
+        <input type="hidden" name="authenticity_token" value="${csrfToken}">
+        <input value="${newTopic.created_date}" type="date" name="past_topic[created_date]" id="past_topic_created_date">              
+        <textarea name="past_topic[past_topic]" id="new_past_topic_input_${newTopic.id}"></textarea>
+      </form>
+      <button type="button" id="delete_new_topic_btn_${newTopic.id}" data="${newTopic.id}">削除</button>
+    </div>
+    `; // newTopic.id同じモノを繰り返しすぎ・・・
   const pastTopics = document.getElementById("past_topics");
   pastTopics.insertAdjacentHTML("afterbegin", html); // 新規フォームをHTMLに挿入
   updateNewTopic(); //処理を外に出したい・・・（async/awaitあたり？）
+  deleteNewTopic(newTopic.id);
   };
 };
 
@@ -49,6 +54,12 @@ const updateNewTopic = () => {
         updateData(newPastTopicInput, "past_topics");
       });
     });
+const deleteNewTopic = (newTopicId) => {
+  const csrfToken = document.querySelector("meta[name='csrf-token']").content;
+  const delete_new_topic_btn = document.getElementById(`delete_new_topic_btn_${newTopicId}`);
+  delete_new_topic_btn.addEventListener("click", function(){
+    const topicId = this.getAttribute("data");
+    deleteData(csrfToken, topicId);
   });
 };
 
