2ece8a05428a767d25dfee7d345efc39fc2468f5
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-29 12:41:34 +0900

    categories#createアクション作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,11 @@
 class CategoriesController < ApplicationController
+  def create
+    @category = Category.new(category_params)
+  end
+
+  private
+  
+  def category_params
+    params.require(:category).permit(:name, :describe)
+  end
 end
