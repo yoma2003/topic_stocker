10fe6ab63a91eead298256491758c8fc77710313
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-29 13:09:51 +0900

    アソシエーションの修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  has_many: characters, through :character_categories
+  has_many :characters, through: :character_categories
 
   validates :name, presence: true
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -4,5 +4,5 @@ class Character < ApplicationRecord
   belongs_to :user
   has_many :past_topics, dependent: :destroy
   has_many :future_topics, dependent: :destroy
-  has_many :categories, through: character_categories, dependent: destroy
+  has_many :categories, through: :character_categories, dependent: :destroy
 end
