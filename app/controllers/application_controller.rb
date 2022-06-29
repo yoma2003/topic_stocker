f9079015a0720adbcc7668bf34293f88f59e7190
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-29 18:32:26 +0900

    applicationコントローラーにbefore_action :set_categoriesを定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :set_categories
+
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def set_categories
+    @categories = current_user.categories
   end
 end
