bfb10d3af5064342c968a9d632c5a166d8f5cc65
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-30 20:39:25 +0900

    categoriesコントローラー編集（edit、updateアクション追加）

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,9 +19,23 @@ class CategoriesController < ApplicationController
     @characters = @category.characters
   end
 
+  def edit
+    @category = Category.find(params[:id])
+    @characters = current_user.characters
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
+  end
+
   private
   
   def category_params
-    params.require(:category).permit(:name, :describe).merge(user_id: current_user.id)
+    params.require(:category).permit(:name, :describe, character_ids: []).merge(user_id: current_user.id)
   end
 end
