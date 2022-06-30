264a893cc5c0281aa1feb1721675791563c4d813
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-06-30 10:35:54 +0900

    character_topicモデル編集

--- app/models/character_topic.rb
+++ app/models/character_topic.rb
@@ -1,6 +1,6 @@
 class CharacterTopic
   include ActiveModel::Model
-  attr_accessor :name, :image, :url, :describe, :past_topic, :created_date, :future_topic, :user_id
+  attr_accessor :name, :image, :url, :describe, :past_topic, :created_date, :future_topic, :user_id, :category_ids
 
   with_options presence: true do
     validates :name
@@ -8,7 +8,7 @@ class CharacterTopic
   end
 
   def save
-    character = Character.create(name: name, url: url, describe: describe, image: image, user_id: user_id)
+    character = Character.create(name: name, url: url, describe: describe, image: image, user_id: user_id, category_ids: category_ids)
     PastTopic.create(past_topic: past_topic, created_date: created_date, character_id: character.id)
     FutureTopic.create(future_topic: future_topic, character_id: character.id)
   end
