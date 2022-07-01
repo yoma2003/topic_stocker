7786eff523e60ebabf6afc7e27fd9159634c5b75
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-07-01 16:51:52 +0900

    categoriesリソース編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "categories#index"
   resources :users, only: [:edit, :update]
-  resources :categories, only: [:create, :show, :edit, :update, :destroy]
+  resources :categories, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :characters
   resources :future_topics, only: :update
   resources :past_topics, only: [:update, :create, :destroy]
