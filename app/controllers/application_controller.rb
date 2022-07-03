35de5211d887a87699872b54c4291a73d7d2de95
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-07-03 16:48:03 +0900

    サンプル画像の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, if: :devise_controller?
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  before_action :set_categories
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
-
-  def set_categories
-    @categories = current_user.categories
   end
 end
