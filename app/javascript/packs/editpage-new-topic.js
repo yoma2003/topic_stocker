18fb37a4c8336e2656ce7bec36ad30a3764d1043
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-07-05 16:03:13 +0900

    不要なid付与処理の削除（リファクタリング）

--- app/javascript/packs/editpage-new-topic.js
+++ app/javascript/packs/editpage-new-topic.js
@@ -29,42 +29,46 @@ const newData = (csrfToken, characterId) => {
       <form class="topic_form" data="${newTopic.id}" action="/past_topics/${newTopic.id}" accept-charset="UTF-8" method="post">
         <input type="hidden" name="_method" value="patch">
         <input type="hidden" name="authenticity_token" value="${csrfToken}">
-        <input value="${newTopic.created_date}" type="date" name="past_topic[created_date]" id="past_topic_created_date">              
-        <textarea name="past_topic[past_topic]" id="new_past_topic_input_${newTopic.id}"></textarea>
+        <input value="${newTopic.created_date}" type="date" name="past_topic[created_date]" class="new_past_topic_input past_topic_create_date" id="past_topic_created_date">              
+        <textarea name="past_topic[past_topic]" class="topic_input new_past_topic_input past_topic_text"></textarea>
       </form>
-      <button type="button" id="delete_new_topic_btn_${newTopic.id}" data="${newTopic.id}">削除</button>
+      <button type="button" class="delete_new_topic_btn" data="${newTopic.id}">削除</button>
     </div>
     `; // newTopic.id同じモノを繰り返しすぎ・・・
   const pastTopics = document.getElementById("past_topics");
   pastTopics.insertAdjacentHTML("afterbegin", html); // 新規フォームをHTMLに挿入
-  updateNewTopic(newTopic.id); //処理を外に出したい・・・（async/awaitあたり？）
-  deleteNewTopic(newTopic.id);
+  updateNewTopic(); //処理を外に出したい・・・（async/awaitあたり？）
+  deleteNewTopic();
   };
 };
 
-const updateNewTopic = (newTopicId) => {
-  const newPastTopicInput = document.getElementById(`new_past_topic_input_${newTopicId}`);
-  newPastTopicInput.addEventListener("blur", function(e){
-    updateData(newPastTopicInput, "past_topics");
-  });
-  newPastTopicInput.addEventListener("focus", function(){
-    console.log("OK");
-    window.addEventListener("beforeunload", function(e){
+const updateNewTopic = () => {
+  const newPastTopicInputs = document.querySelectorAll(".new_past_topic_input");
+  console.log(newPastTopicInputs)
+  newPastTopicInputs.forEach(function(newPastTopicInput){
+    newPastTopicInput.addEventListener("blur", function(e){
       updateData(newPastTopicInput, "past_topics");
+    });
+    newPastTopicInput.addEventListener("focus", function(){
+      window.addEventListener("beforeunload", function(e){
+        updateData(newPastTopicInput, "past_topics");
+      });
     });
   });
 };
 
-const deleteNewTopic = (newTopicId) => {
+const deleteNewTopic = () => {
   const csrfToken = document.querySelector("meta[name='csrf-token']").content;
-  const delete_new_topic_btn = document.getElementById(`delete_new_topic_btn_${newTopicId}`);
-  delete_new_topic_btn.addEventListener("click", function(){
-    const topicId = this.getAttribute("data");
-    if (window.confirm(`話した事データ${topicId}（1件）を削除しますか？`)) {
-      deleteData(csrfToken, topicId);
-    } else {
-      return;
-    }
+  const delete_new_topic_btns = document.querySelectorAll(".delete_new_topic_btn");
+  delete_new_topic_btns.forEach(function(delete_new_topic_btn){
+    delete_new_topic_btn.addEventListener("click", function(){
+      const topicId = this.getAttribute("data");
+      if (window.confirm(`話した事データ${topicId}（1件）を削除しますか？`)) {
+        deleteData(csrfToken, topicId);
+      } else {
+        return;
+      }
+    });
   });
 };
 
