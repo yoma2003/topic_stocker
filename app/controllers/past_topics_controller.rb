4b88d80562f8c7cd76610a3a1552614bfe991801
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-07-05 17:52:24 +0900

    子モデルインスタンスに変更があれば親モデルを更新する処理

--- app/controllers/past_topics_controller.rb
+++ app/controllers/past_topics_controller.rb
@@ -2,16 +2,21 @@ class PastTopicsController < ApplicationController
   def update
     latest_past_topic = PastTopic.find(params[:id])
     latest_past_topic.update(past_topic_params)
+    if latest_past_topic.saved_changes?
+      latest_past_topic.character.touch
+    end
   end
 
   def create
     new_past_topic = PastTopic.create(past_topic_params)
+    new_past_topic.character.touch
     render json: { new_past_topic: new_past_topic }
   end
 
   def destroy
     delete_past_topic = PastTopic.find(params[:id])
     delete_past_topic.destroy
+    delete_past_topic.character.touch
 
     if params[:character_id] #indexpage-delete-topic.jsから来たリクエストの処理
       character = Character.find(character_id_params)
