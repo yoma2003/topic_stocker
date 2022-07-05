9a71b44412f65ad6e0f5a45cf21dd31f78949d63
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-07-05 16:02:02 +0900

    import修正

--- app/javascript/packs/delete-topic.js
+++ app/javascript/packs/delete-topic.js
@@ -1,3 +1,5 @@
+import { deleteData } from "./delete-data";
+
 const deleteTopic = () => {
   const csrfToken = document.querySelector("meta[name='csrf-token']").content;
   const delete_topic_btns = document.querySelectorAll(".delete_topic_btn");
@@ -13,18 +15,4 @@ const deleteTopic = () => {
   });
 };
 
-const deleteData = (csrfToken, topicId) => {
-  const deleteTopicElement = document.getElementById(`past_topic_${topicId}`);
-  const formData = new FormData();
-  formData.set("authenticity_token", `${csrfToken}`);
-  const XHR = new XMLHttpRequest();
-  XHR.open("DELETE", `/past_topics/${topicId}`, true);
-  XHR.send(formData); // 削除リクエストの送信
-  XHR.onload = () => {
-    deleteTopicElement.remove();
-  };
-};
-
 window.addEventListener("load", deleteTopic);
-
-export { deleteData };
