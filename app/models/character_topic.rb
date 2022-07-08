ba0e7a2c6420e07962212bc9c49c850ed0481a19
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-07-08 23:59:06 +0900

    user_idカラムのバリデーション追加

--- app/models/character_topic.rb
+++ app/models/character_topic.rb
@@ -2,8 +2,11 @@ class CharacterTopic
   include ActiveModel::Model
   attr_accessor :name, :image, :url, :describe, :past_topic, :created_date, :future_topic, :user_id, :category_ids
 
-  validates :name, presence: true
-
+  with_options presence: true do
+    validates :name
+    validates :user_id
+  end
+  
   def save
     character = Character.create(name: name, url: url, describe: describe, image: image, user_id: user_id, category_ids: category_ids)
     PastTopic.create(past_topic: past_topic, created_date: created_date, character_id: character.id)
