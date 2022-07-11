549fc6254e00673810e726b581534fb60f364856
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-07-11 23:30:59 +0900

    orderメソッドに２つ目の条件を追加

--- app/models/character.rb
+++ app/models/character.rb
@@ -11,11 +11,11 @@ class Character < ApplicationRecord
 
   scope :order_updated_at, -> (current_user_id) {where(user_id: current_user_id).order(updated_at: :desc)}
   scope :order_past_topic, -> (current_user_id) {
-    left_joins(:past_topics).group(:id).order("MAX(past_topics.created_date) DESC").where(user_id: current_user_id)
+    left_joins(:past_topics).group(:id).order("MAX(past_topics.created_date) DESC, characters.id DESC").where(user_id: current_user_id)
   }
 
   scope :category_order_updated_at, -> (category_id) {joins(:character_categories).where(character_categories: { category_id: category_id }).order(updated_at: :desc)}
   scope :category_order_past_topic, -> (category_id) {
-    left_joins(:past_topics, :character_categories).group(:id).order("MAX(past_topics.created_date) DESC").where(character_categories: { category_id: category_id })
+    left_joins(:past_topics, :character_categories).group(:id).order("MAX(past_topics.created_date) DESC, characters.id DESC").where(character_categories: { category_id: category_id })
   }
 end
