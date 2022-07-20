a4086048a3ad270e06a5053c6ee12ff843313f6b
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-07-20 12:22:01 +0900

    自動保存時間の表示処理の作成

--- app/javascript/packs/update-topic.js
+++ app/javascript/packs/update-topic.js
@@ -6,23 +6,38 @@ const updateTopic = () => {
   pastTopicInputs.forEach(function(pastTopicInput){
     pastTopicInput.addEventListener("blur", function(e){
       updateData(pastTopicInput, "past_topics");
+      updateTime();
     });
     pastTopicInput.addEventListener("focus", function(){
       window.addEventListener("beforeunload", function(e){
         updateData(pastTopicInput, "past_topics");
+        updateTime();
       });
     });
   });
   futureTopicInputs.forEach(function(futureTopicInput){
     futureTopicInput.addEventListener("blur", function(e){
       updateData(futureTopicInput, "future_topics");
+      updateTime();
     });
     futureTopicInput.addEventListener("focus", function(){
       window.addEventListener("beforeunload", function(e){
         updateData(futureTopicInput, "future_topics");
+        updateTime();
       });
     });
   });
 };
+
+const updateTime = () => {
+  const updateTime = document.getElementById("update_time");
+  const todayData = new Date();
+  const html = `${todayData.getFullYear()}/${todayData.getMonth() + 1}/${todayData.getDate()} ${twoDigitsNum(todayData.getHours())}:${twoDigitsNum(todayData.getMinutes())} Topic自動保存`;
+  updateTime.innerHTML = html;
+};
+
+const twoDigitsNum = (num) => {
+  return ( '00' + num ).slice( -2 );
+}
 
 window.addEventListener("load", updateTopic);
