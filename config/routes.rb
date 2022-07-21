e93c040bd3d88520c72bc94d62eed2f9277fbdb6
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-07-21 10:34:57 +0900

    ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  
   root to: "categories#index"
   resources :users, only: [:edit, :update]
   resources :categories, only: [:new, :create, :show, :edit, :update, :destroy]
