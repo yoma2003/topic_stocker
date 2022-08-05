1350d9fc8240535e6fca79d13d635aff5f830199
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-08-05 13:11:10 +0900

    アラートメッセージ削除

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,6 @@ class Users::SessionsController < Devise::SessionsController #Deviseコントロ
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to root_path, alert: "ゲストユーザーでログインしています。データは定期的に初期化されます。"
+    redirect_to root_path
   end
 end
