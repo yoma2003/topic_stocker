0d5128dd2d193242288e218c76f7d1bef3d00d11
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-08-28 23:54:05 +0900

    ログアウト後の遷移ページカスタマイズ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
-  def configure_permitted_parameters
+  def configure_permitted_parameters #deviseのストロングパラメーター
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def after_sign_out_path_for(resource) #deviseのログアウト後に繊維するページの指定
+    new_user_session_path
   end
 end
