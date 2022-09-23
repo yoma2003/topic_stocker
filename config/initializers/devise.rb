33c1f550a5c5296d156d02124cd7fffc716da4a2
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-09-23 22:35:54 +0900

    deviseイニシャライザーにomniauth設定

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,6 +273,9 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  # GoogleOauthの環境変数読み込み設定
+  config.omniauth :google_oauth2,ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET']
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
