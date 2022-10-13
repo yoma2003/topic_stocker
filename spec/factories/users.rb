1beb7645a93c2ff81772564bbd44ea0ebeec8d65
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-10-13 23:14:07 +0900

    UserモデルのFactoryBot改修

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,11 +4,18 @@ FactoryBot.define do
     email                 { Faker::Internet.free_email }
     password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
+    uid                   { "123456878901234567890" }
+    provider              { "google" }
+    image                 { Rack::Test::UploadedFile.new(Rails.root.join("app/assets/images/guest_image.jpg"), 'image/jpeg') }
   end
 
-  trait :categories do
-    after(:create) do |user|
-      user.categories = create_list(:category, 5, user: user)
-    end
-  end
+  # after(:build) do |user|
+  #   user.image.attach(io:File.open('app/assets/images/guest_image.jpg'), filename: 'guest_image.jpg')
+  # end
+  
+  # trait :categories do
+  #   after(:create) do |user|
+  #     user.categories = create_list(:category, 5, user: user)
+  #   end
+  # end
 end
