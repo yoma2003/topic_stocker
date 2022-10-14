fc7bde47e785fa514b01be386d0c3239affdab48
yoma2003 <102401730+yoma2003@users.noreply.github.com>
2022-10-14 23:47:30 +0900

    メソッド微修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,12 +6,8 @@ FactoryBot.define do
     password_confirmation { password }
     uid                   { "123456878901234567890" }
     provider              { "google" }
-    image                 { Rack::Test::UploadedFile.new(Rails.root.join("app/assets/images/guest_image.jpg"), 'image/jpeg') }
+    image                 { fixture_file_upload('app/assets/images/guest_image.jpg', 'image/jpeg') }
   end
-
-  # after(:build) do |user|
-  #   user.image.attach(io:File.open('app/assets/images/guest_image.jpg'), filename: 'guest_image.jpg')
-  # end
   
   # trait :categories do
   #   after(:create) do |user|
